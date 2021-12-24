d1c522003a2b49593cb292819e19bf3685d8428f
YanWQ-monad <YanWQmonad@gmail.com>
2021-12-24 21:26:27 +0800

    init: initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prosthemadera',
+    version='0.0.1',
+    packages=find_packages(include='prosthemadera.*'),
+    url='https://github.com/YanWQ-monad/prosthemadera',
+    license='MIT',
+    author='YanWQmonad',
+    author_email='YanWQmonad@gmail.com',
+    description=''
+)
